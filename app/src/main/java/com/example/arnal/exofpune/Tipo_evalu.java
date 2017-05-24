162917ea1fd9f5637cd7d2455a73afc32a615944
arnaldo1ooo <arnaldo1ooo@hotmail.com>
2017-05-24 13:09:16 -0400

    Commit

--- app/src/main/java/com/example/arnal/exofpune/Tipo_evalu.java
+++ app/src/main/java/com/example/arnal/exofpune/Tipo_evalu.java
@@ -3,12 +3,19 @@ package com.example.arnal.exofpune;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Tipo_evalu extends AppCompatActivity {
     Button btnopc1;
     Button btnopc2;
+
 
 
 
@@ -24,16 +31,30 @@ public class Tipo_evalu extends AppCompatActivity {
         btnopc1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), activity_evalu1.class);
-                startActivityForResult(intent, 0);
+                Date FechaActual = new Date();
+                SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+                Date FechaDate = null;
+                try {
+                    FechaDate = formato.parse("15/06/2017");
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                if (FechaActual.before(FechaDate)){
+                    Intent intent = new Intent (v.getContext(), activity_evalu1.class);
+                    startActivityForResult(intent, 0);
+                }
+                else{
+                    Log.d("myTag", "Ya caducó");
+                }
+
             }
         });
 
         btnopc2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), activity_evalu2.class);
-                startActivityForResult(intent, 0);
+
             }
         });
     }
