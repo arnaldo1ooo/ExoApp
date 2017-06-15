32909b73f08f904836783575d452dae0390bb53f
arnaldo1ooo <arnaldo1ooo@hotmail.com>
2017-06-15 16:05:43 -0400

    Commit

--- app/src/main/java/com/exofpune/activity_acercade.java
+++ app/src/main/java/com/exofpune/activity_acercade.java
@@ -1,15 +1,30 @@
 package com.exofpune;
 
+import android.content.pm.PackageInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.exofpune.R;
 
 public class activity_acercade extends AppCompatActivity {
 
+    TextView version;
+    String strVersion;
+    PackageInfo packageInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acercade);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        version = (TextView) findViewById(R.id.tvVersion);
+
+
+
+        version.setText("");
     }
 }
