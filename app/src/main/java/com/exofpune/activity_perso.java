a4dc68d6caa1c74f9de88fbf23335b56d7e3d226
arnaldo1ooo <arnaldo1ooo@hotmail.com>
2017-06-16 14:07:03 -0400

    Commit

--- app/src/main/java/com/exofpune/activity_perso.java
+++ app/src/main/java/com/exofpune/activity_perso.java
@@ -2,10 +2,15 @@ package com.exofpune;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.exofpune.R;
 
 public class activity_perso extends AppCompatActivity {
+
+
+    Button tbTp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,21 @@ public class activity_perso extends AppCompatActivity {
         setContentView(R.layout.activity_perso);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        tbTp = (Button) findViewById(R.id.tbTp);
+
+
+
+
+
+
+        tbTp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
