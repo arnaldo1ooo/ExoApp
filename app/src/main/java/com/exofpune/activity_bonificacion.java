17f8a877d96cfb1443c6e98be642b0b3b94d2e84
arnaldo1ooo <arnaldo1ooo@hotmail.com>
2019-06-17 10:34:16 -0400

    PRobando

--- app/src/main/java/com/exofpune/activity_bonificacion.java
+++ app/src/main/java/com/exofpune/activity_bonificacion.java
@@ -1,17 +1,14 @@
 package com.exofpune;
 
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class activity_bonificacion extends AppCompatActivity {
 
@@ -26,7 +23,7 @@ public class activity_bonificacion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_acercade);
+        setContentView(R.layout.activity_bonificacion);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Bonificación total");
@@ -44,7 +41,12 @@ public class activity_bonificacion extends AppCompatActivity {
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calcular();
+                Log.d("etBonificacion ",etBonificacion.getText().toString());
+                if (etBonificacion.getText().equals(null) == false){
+                    Calcular();
+                }else{
+                    Toast.makeText(null, "La bonificación no es válida",Toast.LENGTH_LONG);
+                }
             }
         });
 
@@ -60,25 +62,28 @@ public class activity_bonificacion extends AppCompatActivity {
             tvNota5.setVisibility(TextView.VISIBLE);
         }
         if (valor == "INVISIBLE"){
-            //tvTitulo2.setVisibility(View.INVISIBLE);
+            tvTitulo2.setVisibility(View.INVISIBLE);
             tvNota2.setVisibility(View.INVISIBLE);
-            //tvNota3.setVisibility(View.INVISIBLE);
-            //tvNota4.setVisibility(TextView.INVISIBLE);
-            //tvNota5.setVisibility(TextView.INVISIBLE);
+            tvNota3.setVisibility(View.INVISIBLE);
+            tvNota4.setVisibility(TextView.INVISIBLE);
+            tvNota5.setVisibility(TextView.INVISIBLE);
         }
     }
 
-    double TotalFinal = 60;
-    double Minpara2 = 60;
-    double Minpara3 = 60;
-    double Minpara4 = 60;
-    double Minpara5 = 60;
+
     public void Calcular() {
+        DecimalFormat df = new DecimalFormat("#.###");
+        double TotalFinal = 60;
+        double Minpara2 = 60;
+        double Minpara3 = 60;
+        double Minpara4 = 60;
+        double Minpara5 = 60;
+
         PonerObjetosInvisibles("VISIBLE");
         double Bonif = Double.parseDouble(etBonificacion.getText().toString());
 
         double Nota2 = Minpara2 - Bonif;
         double Nota2Porc = (Nota2*100)/Minpara2;
-        tvNota2.setText("Para nota 2: " + Nota2 +" puntos de " + TotalFinal + " (" + Nota2Porc + "% de 100%)");
+        tvNota2.setText(df.format(Nota2) +" puntos de " + TotalFinal + " (" + df.format(Nota2Porc) + "% de 100%)");
     }
 }
